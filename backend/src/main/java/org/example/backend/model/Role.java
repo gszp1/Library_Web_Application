e2667ddfb64c36f45fde7db4bad3ca76c80bc4a3
gszp1 <grzybekmateusz@poczta.fm>
2024-04-07 21:10:28 +0200

    Adds Role entity.

---
+++ backend/src/main/java/org/example/backend/model/Role.java
@@ -0,0 +1,16 @@
+package org.example.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
