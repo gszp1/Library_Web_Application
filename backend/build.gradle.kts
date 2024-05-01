dc5554753ac157b0828a59fafe7fcb7eb171e453
gszp1 <grzybekmateusz@poczta.fm>
2024-05-01 23:48:43 +0200

    Adds Swagger dependency.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 }
 
 tasks.withType<Test> {
