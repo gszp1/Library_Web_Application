549f75955f4311832415729700676d5780e5e8cf
gszp1 <grzybekmateusz@poczta.fm>
2024-05-03 01:42:16 +0200

    Adds Publisher entity.

---
+++ backend/src/main/java/org/example/backend/model/Publisher.java
@@ -0,0 +1,23 @@
+package org.example.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "publisher_id")
+    private int publisherId;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String address;
+}
