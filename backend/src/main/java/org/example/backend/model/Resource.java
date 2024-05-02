abda3d77a081d23c5da0058747a8bc7dedd2c623
gszp1 <grzybekmateusz@poczta.fm>
2024-05-02 23:31:49 +0200

    Adds Resoruce entity.

---
+++ backend/src/main/java/org/example/backend/model/Resource.java
@@ -0,0 +1,19 @@
+package org.example.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
