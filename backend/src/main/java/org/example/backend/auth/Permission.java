6e18d65a75b19458bde483c0869171af84aa3f14
gszp1 <grzybekmateusz@poczta.fm>
2024-05-24 08:55:56 +0200

    Adds Permissions and Roles.

--- backend/src/main/java/org/example/backend/auth/Permission.java
+++ backend/src/main/java/org/example/backend/auth/Permission.java
@@ -7,11 +7,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Permission {
     ADMIN_READ("admin:read"),
-    ADMIN_WRITE("admin:write"),
+    ADMIN_UPDATE("admin:update"),
     ADMIN_CREATE("admin:create"),
     ADMIN_DELETE("admin:delete"),
     USER_READ("user:read"),
-    USER_WRITE("user:write"),
+    USER_UPDATE("user:update"),
     USER_CREATE("user:create"),
     USER_DELETE("user:delete");
 
--- backend/src/main/java/org/example/backend/auth/Role.java
+++ backend/src/main/java/org/example/backend/auth/Role.java
@@ -1,6 +1,32 @@
 package org.example.backend.auth;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import java.util.Set;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    USER,
-    ADMIN
+    USER(
+            Set.of(
+                    Permission.USER_CREATE,
+                    Permission.USER_READ,
+                    Permission.USER_UPDATE,
+                    Permission.USER_DELETE
+            )
+    ),
+    ADMIN(
+            Set.of(
+                    Permission.ADMIN_CREATE,
+                    Permission.ADMIN_READ,
+                    Permission.ADMIN_UPDATE,
+                    Permission.ADMIN_DELETE,
+                    Permission.USER_CREATE,
+                    Permission.USER_READ,
+                    Permission.USER_UPDATE,
+                    Permission.USER_DELETE
+            )
+    );
+
+    private final Set<Permission> permissions;
 }
