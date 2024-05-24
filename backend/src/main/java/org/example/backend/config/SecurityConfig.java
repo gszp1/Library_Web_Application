54973e0ae2cf9a29a0224db3b165764dc6e498ad
gszp1 <grzybekmateusz@poczta.fm>
2024-05-24 09:03:13 +0200

    Adds SecurityConfig.

---
+++ backend/src/main/java/org/example/backend/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package org.example.backend.config;
+
+public class SecurityConfig {
+
+    public final static String SECRET_KEY = "645120434373653647323c5e484d2a505a29335d4d2e757a4c6c497545";
+
+}
