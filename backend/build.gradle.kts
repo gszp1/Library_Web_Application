c24e9c5623ef6886e60d0a711b7db600b714a913
gszp1 <grzybekmateusz@poczta.fm>
2024-05-24 10:47:40 +0200

    Adds required annotations.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,9 +25,13 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.2.5")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("org.springframework:spring-webmvc")
+	implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
@@ -37,10 +41,15 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 
 	compileOnly("org.projectlombok:lombok")
+	compileOnly("jakarta.servlet:jakarta.servlet-api")
 
 	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
