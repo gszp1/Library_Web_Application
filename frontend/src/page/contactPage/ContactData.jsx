3f41d1c50fe523a8aeb828c560e91970817c15c3
gszp1 <grzybekmateusz@poczta.fm>
2024-06-06 02:04:11 +0200

    Adds displaying contact informations.

--- frontend/src/page/contactPage/ContactData.jsx
+++ frontend/src/page/contactPage/ContactData.jsx
@@ -1,10 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faMapMarkerAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "./ContactPageStyles.css";
 
-function ContactData({phoneNumber, address, email}) {
+function ContactData({phoneNumber="1", address="1", email="1"}) {
     return (
-        <>
-        </>
+        <div className="contactData">
+            <div className="contactHeader">Contact Information</div>
+            <div className="contactItem">
+                <strong>
+                    <FontAwesomeIcon icon={faPhone} className="icon"/>
+                    {" Phone Number:"}
+                </strong>
+                <span>{phoneNumber}</span>
+            </div>
+            <div className="contactItem">
+                <strong>
+                    <FontAwesomeIcon icon={faMapMarkerAlt} className="icon"/>
+                    {" Address:"}
+                </strong>
+                <span>{address}</span>
+            </div>
+            <div className="contactItem">
+                <strong>
+                    <FontAwesomeIcon icon={faEnvelope} className="icon"/>
+                    {" Email:"}
+                </strong>
+                <span>{email}</span>
+            </div>
+        </div>
     );
 }
 
