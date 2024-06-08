eb286c73c386b0630703f51e6074d23c36cb39eb
gszp1 <grzybekmateusz@poczta.fm>
2024-06-08 14:26:25 +0200

    Adds content layout for ReosurcePage.

---
+++ frontend/src/page/resourcesPage/ResourcePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ResourcePage(){
+    <div>
+        <div>
+            <img></img>
+            <li>
+                
+            </li>
+        </div>
+        <p>
+
+        </p>
+        <button>
+
+        </button>
+    </div>
+}
+
+export default ResourcePage;
