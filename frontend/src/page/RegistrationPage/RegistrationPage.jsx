43ab06fa275c9574a626795fb78bcebd34ff126b
gszp1 <grzybekmateusz@poczta.fm>
2024-06-09 13:36:05 +0200

    Adds prompts.

--- frontend/src/page/RegistrationPage/RegistrationPage.jsx
+++ frontend/src/page/RegistrationPage/RegistrationPage.jsx
@@ -2,7 +2,23 @@ import React, { useState } from "react";
 import "./RegistrationPageStyles.css";
 
 function RegistrationPage() {
-    const [credentials, setCredentials] = useState(null);
+    const [credentials, setCredentials] = useState({
+        email: '',
+        emailConfirmation: '',
+        password: '',
+        passwordConfirmation: '',
+        name: '',
+        surname: '',
+        phone: ''
+    });
+
+    const [prompts, setPrompts] = useState ({
+        email: '',
+        emailConfirmation: '',
+        password: '',
+        passwordConfirmation: '',
+        phone: ''
+    })
 
     return (
         <form className="registrationForm">
@@ -12,12 +28,16 @@ function RegistrationPage() {
                     <h2>Required Fields</h2>
                     <label>E-Mail</label>
                     <input></input>
+                    <p id="emailPrompt"></p>
                     <label>Confirm E-Mail</label>
                     <input></input>
+                    <p id="emailConfirmPrompt"></p>
                     <label>Password</label>
                     <input></input>
+                    <p id="passwordPrompt"></p>
                     <label>Confirm Password</label>
                     <input></input>
+                    <p id="passwordConfirmPrompt"></p>
                 </div>
                 <div className="optionalInputs">
                     <h2>Optional Fields</h2>
@@ -27,6 +47,7 @@ function RegistrationPage() {
                     <input></input>
                     <label>Phone Number</label>
                     <input></input>
+                    <p id="phonePrompt"></p>
                 </div>
             </div>
             <button>Create account</button>
