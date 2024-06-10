dddf05171d06da78d741ebe885609e48c26c29fd
gszp1 <grzybekmateusz@poczta.fm>
2024-06-10 17:27:25 +0200

    Adds rendering based on state.

--- frontend/src/page/AccountPage/AccountPage.jsx
+++ frontend/src/page/AccountPage/AccountPage.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
 import './AccountPageStyles.css';
+import InformationSection from "./InformationSection";
+import ReservationsSection from "./ReservationsSection";
 
 function AccountPage() {
-    const [selectedSection, setSelectedSection] = useState("Information")
+    const [selectedSection, setSelectedSection] = useState('Information');
+
+    const renderSection = () => {
+        switch (selectedSection) {
+            case 'Information':
+                return <InformationSection />
+            case 'Reservations':
+                return <ReservationsSection />
+        }
+    }
 
     return (
         <div className="pageContent">
@@ -14,6 +25,7 @@ function AccountPage() {
                     <p>Reservations</p>
                 </li>
             </ul>
+            {renderSection()}
         </div>
     );
 }
