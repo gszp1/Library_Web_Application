d49034807db30e101390b7afcb07bebcbd62d4b2
gszp1 <grzybekmateusz@poczta.fm>
2024-06-11 01:44:24 +0200

    Fixes for homePage styling.

--- frontend/src/page/homePage/HomePage.jsx
+++ frontend/src/page/homePage/HomePage.jsx
@@ -3,7 +3,7 @@ import './HomePageStyles.css';
 
 function HomePage() {
   return (
-    <div className='pageContent'>
+    <div className='homePageContent'>
       <h1>Home Page</h1>
       <p>Welcome to the Home Page!</p>
     </div>
