a3ae7dd0667b5ccd1bc5abca2e48b480294a0560
gszp1 <grzybekmateusz@poczta.fm>
2024-06-11 20:15:11 +0200

    Adds ReservationRepository.

--- backend/src/main/java/org/example/backend/service/ReservationService.java
+++ backend/src/main/java/org/example/backend/service/ReservationService.java
@@ -1,8 +1,17 @@
 package org.example.backend.service;
 
+import org.example.backend.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
-    
+
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
 }
