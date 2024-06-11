56cffefd6090eafb583593f0c702f8767413d66c
gszp1 <grzybekmateusz@poczta.fm>
2024-06-11 16:08:09 +0200

    Adds forms.

--- frontend/src/page/AccountPage/ModifyInformationSection.jsx
+++ frontend/src/page/AccountPage/ModifyInformationSection.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ModifyInformationSection() {
+function ModifyInformationSection({credentials, setCredentials}) {
+    const [newCredentials, setNewCredentials] = useState({...credentials})
+    
+    const [prompts, setPrompts] = useState({
+        result: {
+            message: '',
+            color: ''
+        }
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewCredentials({
+            ...newCredentials,
+            [name]: value
+        });
+    };
+
     return (
         <div className="accountPageSection">
             <h1>Modify Information</h1>
-            <form className='credentialUpdateForm'>
+            <form
+                className='credentialUpdateForm'
+                onSubmit={handleSubmit}
+            >
                 <label>Name</label>
                 <input
                     name="name"
                     type="text"
+                    value={newCredentials.name}
+                    onChange={handleChange}
                  />
                 <label>Surname</label>
                 <input
                     name="surname"
                     type="text"
+                    value={newCredentials.surname}
+                    onChange={handleChange}
                 />
                 <label>Phone Number</label>
                 <input
                     name="phoneNumber"
                     type="tel"
+                    value={newCredentials.phoneNumber}
+                    onChange={handleChange}
                 />
-                <button>Submit</button>
+                <button>
+                    Submit
+                </button>
+                <p style={{color: prompts.result.color}}>
+                    {prompts.result.message}
+                </p>
             </form>
         </div>
     );
