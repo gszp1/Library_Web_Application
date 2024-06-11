d8703a87eaaa1a0315c058da935017865b6de8f0
gszp1 <grzybekmateusz@poczta.fm>
2024-06-11 09:39:22 +0200

    Adds state to informations section.

--- frontend/src/page/AccountPage/InformationSection.jsx
+++ frontend/src/page/AccountPage/InformationSection.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './AccountPageStyles.css';
 
 function InformationSection() {
+    const [userData, setUserData] = useState({
+        name: '',
+        surname: '',
+        phoneNumber: '',
+        email: '',
+        imageUrl: ''
+    });
+
+    // useEffect (async () => {
+    //     let url = ''
+    // }, []);
+
     return (
         <div className='accountPageSection'>
             <h1> Account Information </h1>
