d70353681869932c69bb309f2a48fd7af64f73a6
gszp1 <grzybekmateusz@poczta.fm>
2024-06-11 22:58:32 +0200

    Adds finding not reserved instances by Resource id.

--- backend/src/main/java/org/example/backend/repository/ResourceInstanceRepository.java
+++ backend/src/main/java/org/example/backend/repository/ResourceInstanceRepository.java
@@ -4,8 +4,10 @@ import org.example.backend.model.ResourceInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResourceInstanceRepository extends JpaRepository<ResourceInstance, Integer> {
 
-
+    List<ResourceInstance> findByResourceResourceIdAndIsReservedFalse(Integer resourceId);
 }
