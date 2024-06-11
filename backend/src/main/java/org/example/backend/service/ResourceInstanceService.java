1a5b396078470f3d28fb76e9b7e644d9250c366e
gszp1 <grzybekmateusz@poczta.fm>
2024-06-11 23:47:58 +0200

    Adds getNotReservedInstancesOfResource method.

--- backend/src/main/java/org/example/backend/service/ResourceInstanceService.java
+++ backend/src/main/java/org/example/backend/service/ResourceInstanceService.java
@@ -1,8 +1,12 @@
 package org.example.backend.service;
 
+import org.example.backend.dto.InstanceDto;
 import org.example.backend.repository.ResourceInstanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ResourceInstanceService {
@@ -12,5 +16,16 @@ public class ResourceInstanceService {
     @Autowired
     public ResourceInstanceService(ResourceInstanceRepository resourceInstanceRepository) {
         this.resourceInstanceRepository = resourceInstanceRepository;
+    }
+
+    List<InstanceDto> getNotReservedInstancesOfResource(Integer resourceId) {
+        return resourceInstanceRepository
+                .findByResourceResourceIdAndIsReservedFalse(resourceId)
+                .stream()
+                .map(instance -> new InstanceDto(
+                        instance.getResourceInstanceId(),
+                        resourceId,
+                        instance.getIsReserved())
+                ).collect(Collectors.toList());
     }
 }
