e88546436ca1bfa1e4a310bddf2d34579ddb782d
gszp1 <grzybekmateusz@poczta.fm>
2024-06-12 22:10:12 +0200

    Adds exceptions and updates ReservationStatus.

--- backend/src/main/java/org/example/backend/util/ReservationStatus.java
+++ backend/src/main/java/org/example/backend/util/ReservationStatus.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ReservationStatus {
-    PENDING,
-    CONFIRMED,
-    CANCELLED
+    ACTIVE,
+    CANCELLED,
+    COMPLETED,
+    EXPIRED
 }
---
+++ backend/src/main/java/org/example/backend/util/exception/NoSuchInstanceException.java
@@ -0,0 +1,4 @@
+package org.example.backend.util.exception;
+
+public class NoSuchInstanceException extends Exception{
+}
