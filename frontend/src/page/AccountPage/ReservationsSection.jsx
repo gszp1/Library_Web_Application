7f3a5bc3e8f149086cd42f3b0908257884013182
gszp1 <grzybekmateusz@poczta.fm>
2024-06-14 10:25:59 +0200

    Adds state to reservations section.

--- frontend/src/page/AccountPage/ReservationsSection.jsx
+++ frontend/src/page/AccountPage/ReservationsSection.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './AccountPageStyles.css';
 
 function ReservationsSection() {
+    const [reservations, setReservations] = useState[[]];
+
+    useEffect({
+    }, [reservations])
+
     return (
         <div className='accountPageSection'>
             <h1> Reservations </h1>
+
         </div>
     );
 }
