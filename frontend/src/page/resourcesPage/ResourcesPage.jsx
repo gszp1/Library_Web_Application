4aa6848dd432f461dbe48863da7e8571ace5dbdd
gszp1 <grzybekmateusz@poczta.fm>
2024-06-15 20:06:20 +0200

    Adds dummies and adjusts styling.

--- frontend/src/page/resourcesPage/ResourcesPage.jsx
+++ frontend/src/page/resourcesPage/ResourcesPage.jsx
@@ -5,55 +5,61 @@ import useDebounce from "../../customHooks/useDebounce";
 import axios from "axios";
 
 function ResourcesPage({searchKeyword}) {
-    const [resources, setResources] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [page, setPage] = useState(0);
-    const [totalPages, setTotalPages] = useState(0);
-    const [pageSize, setPageSize] = useState(10);
-    const debouncedSearchKeyword = useDebounce(searchKeyword, 1000);
-    useEffect(() => {
-        const fetchResources = async () => {
-            try {
-                let url = `http://localhost:9090/api/resources/all/paginated`;
-                const params = {
-                  keyword: debouncedSearchKeyword,
-                  page: page,
-                  size: pageSize
-                };
-                const response = await axios.get(url, {params});
-                console.log(response);
-                setResources(response.data.content);
-                setTotalPages(response.data.totalPages);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-                setLoading(false);
-            }
-        };
-        fetchResources();
-    }, [debouncedSearchKeyword, page]);
+  const [resources, setResources] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const debouncedSearchKeyword = useDebounce(searchKeyword, 1000);
+  useEffect(() => {
+      const fetchResources = async () => {
+          try {
+              let url = `http://localhost:9090/api/resources/all/paginated`;
+              const params = {
+                keyword: debouncedSearchKeyword,
+                page: page,
+                size: pageSize
+              };
+              const response = await axios.get(url, {params});
+              console.log(response);
+              setResources(response.data.content);
+              setTotalPages(response.data.totalPages);
+              setLoading(false);
+          } catch (error) {
+              setError(error);
+              setLoading(false);
+          }
+      };
+      fetchResources();
+  }, [debouncedSearchKeyword, page]);
 
-    return (
-        <div className="resourcesPageContent">
-          {loading ? (
-            <div>Loading library resources...</div>
-          ) : error ? (
-            <div>Failed to load library resources.</div>
-          ) : resources.length === 0 ? (
-            <div>No library resources available.</div>
-          ) : (
-            resources.map((resource) => (
-              <div key={resource.id}>
-                <Resource
-                  id={resource.id}
-                  resource={resource}
-                />
-              </div>
-            ))
-          )}
-        </div>
-      );
+  const dummyItemsNumber = pageSize - resources.length;
+
+  return (
+    <div className="resourcesPageContent">
+      {loading ? (
+        <div>Loading library resources...</div>
+      ) : error ? (
+        <div>Failed to load library resources.</div>
+      ) : resources.length === 0 ? (
+        <div>No library resources available.</div>
+      ) : (
+        <>
+        {resources.map((resource) => (
+            <Resource
+              id={resource.id}
+              resource={resource}
+              key={resource.id}
+            />
+        ))}
+        {Array.from({length: dummyItemsNumber}).map((_, index) => (
+          <div className='resourceDummy' key={`dummy${index}`}></div>
+        ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ResourcesPage;
