72187950802c8ff99ebfa957267433107667fb83
gszp1 <grzybekmateusz@poczta.fm>
2024-06-16 17:26:20 +0200

    Adds UserIdAndEmailPicker

--- frontend/src/page/AdminPanelPage/UsersSection.jsx
+++ frontend/src/page/AdminPanelPage/UsersSection.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import UserIdAndEmailPicker from "./components/UserIdAndEmailPicker";
 
 function UsersSection() {
+    const [userEmail, setUserEmail] = useState('');
+    const [userId, setUserId] = useState('');
+
     return (
         <div className='adminPanelSection'>
             <h1>Users</h1>
+            <UserIdAndEmailPicker
+                userId={userId}
+                userEmail={userEmail}
+                setUserId={setUserId}
+                setUserEmail={setUserEmail}
+            />
         </div>
     );
 }
