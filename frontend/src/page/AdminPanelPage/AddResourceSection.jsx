2ef44b79d15b1811b0ae0a488050c53f3a9ae6ac
gszp1 <grzybekmateusz@poczta.fm>
2024-06-18 02:31:42 +0200

    Adds AddResourceSection.

--- frontend/src/page/AdminPanelPage/AddResourceSection.jsx
+++ frontend/src/page/AdminPanelPage/AddResourceSection.jsx
@@ -1,9 +1,120 @@
-import React from "react";
+import React, {useState} from "react";
 
 function AddResourceSection() {
+    const [credentials, setCredentials] = useState({
+        title: '',
+        identifier: '',
+        description: '',
+        publisher: '',
+        authors: [{ firstName: '', lastName: '' }]
+    });
+
+    const [image, setImage] = useState(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleAuthorChange = (index, e) => {
+        const { name, value } = e.target;
+        const newAuthors = credentials.authors.map((author, i) => {
+            if (i === index) {
+                return { ...author, [name]: value };
+            }
+            return author;
+        });
+        setCredentials({ ...credentials, authors: newAuthors });
+    };
+
+    const addAuthor = () => {
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            authors: [...prevCredentials.authors, { firstName: '', lastName: '' }]
+        }));
+    };
+
+    const removeAuthor = (index) => {
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            authors: prevCredentials.authors.filter((_, i) => i !== index)
+        }));
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
+    };
+
     return (
         <div className="adminPanelSection">
             <h1>Add Resource</h1>
+            <form onSubmit={handleSubmit} className="resourceCreateForm">
+                <label>Title: </label>
+                <input
+                    name="title"
+                    value={credentials.title}
+                    onChange={handleChange}
+                />
+                <label>Identifier: </label>
+                <input
+                    name="identifier"
+                    value={credentials.identifier}
+                    onChange={handleChange}
+                />
+                <label>Description: </label>
+                <textarea
+                    name="description"
+                    value={credentials.description}
+                    onChange={handleChange}
+                />
+                <label>Publisher: </label>
+                <input
+                    name="publisher"
+                    value={credentials.publisher}
+                    onChange={handleChange}
+                />
+                <h2 style={{color:'orange'}}>Authors:</h2>
+                {credentials.authors.map((author, index) => (
+                    <div key={index} className="authorFields">
+                        <label>Author {index + 1}:</label>
+                        <input
+                            name="firstName"
+                            placeholder="Name"
+                            value={author.name}
+                            onChange={(e) => handleAuthorChange(index, e)}
+                        />
+                        <input
+                            name="lastName"
+                            placeholder="Surname"
+                            value={author.surname}
+                            onChange={(e) => handleAuthorChange(index, e)}
+                        />
+                        <button type="button" onClick={() => removeAuthor(index)}>
+                            Remove
+                        </button>
+                    </div>
+                ))}
+                <button type="button" onClick={addAuthor}>
+                    Add Author
+                </button>
+                <label>Image: </label>
+                <input
+                    style={{marginBottom:'1rem', border:'none', backgroundColor:'transparent', boxShadow:'none', height:'2rem'}}
+                    type="file"
+                    name="image"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                />
+                <button>Submit</button>
+            </form>
         </div>
     );
 }
