88e7f9f21db6c5129ffd1fec8c7b8fab3facc7de
gszp1 <grzybekmateusz@poczta.fm>
2024-06-18 10:57:31 +0200

    Changes.

--- frontend/src/page/AdminPanelPage/AuthorsSection/AddAuthorSection.jsx
+++ frontend/src/page/AdminPanelPage/AuthorsSection/AddAuthorSection.jsx
@@ -1,28 +1,63 @@
 import React, {useState} from "react";
 import '../AdminPanelStyles.css';
 import './AuthorsSectionStyles.css';
+import ReservationPrompt from "../../../component/ReservationPrompt";
 
 function AddAuthorSection({setSection}) {
     const [author, setAuthor] = useState({
-        
+        firstName: '',
+        lastName: '',
+        email: ''
     });
+    const [showPrompt, setShowPrompt] = useState(false)
+    const [promptContent, setPromptContent] = useState({
+        error: false,
+        message: ''
+    })
+
     const handleSubmit = (e) => {
         e.preventDefault();
     }
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAuthor((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
     return (
+        <>
         <div className='adminPanelSection'>
             <h1>Add Author</h1>
             <form className="addAuthorForm" onSubmit={handleSubmit}>
                 <label> Name </label>
-                <input/>
+                <input
+                    type="text"
+                    name="firstName"
+                    value={author.firstName}
+                    onChange={handleChange}
+                />
                 <label> Surname</label>
-                <input/>
+                <input
+                    type="text"
+                    name="lastName"
+                    value={author.lastName}
+                    onChange={handleChange}
+                />
                 <label> E-Mail </label>
-                <input/>
+                <input
+                    type="text"
+                    name="email"
+                    value={author.email}
+                    onChange={handleChange}
+                />
                 <button> Submit </button>
             </form>
         </div>
+        {showPrompt && <ReservationPrompt error={promptContent.error} message={promptContent.message}/>}
+        </>
     );
 }
 
