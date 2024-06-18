37f31695d9eba3348fa3f72892405ee00378c69a
gszp1 <grzybekmateusz@poczta.fm>
2024-06-18 12:18:35 +0200

    Adds dtos.

--- backend/src/main/java/org/example/backend/service/PublisherService.java
+++ backend/src/main/java/org/example/backend/service/PublisherService.java
@@ -18,7 +18,7 @@ public class PublisherService {
     }
 
     public void createPublisher(PublisherDto dto) throws OperationNotAvailableException {
-        if (dto.name()== null|| dto.address() == null) {
+        if (dto.name() == null || dto.address() == null) {
             throw new OperationNotAvailableException("Not all required fields are provided.");
         }
         if (dto.name().isBlank() || dto.address().isBlank()) {
