20c12bf01b5588b2bcaa482570aae59e64e7e881
gszp1 <grzybekmateusz@poczta.fm>
2024-06-19 13:13:48 +0200

    Adds Statistics section and adds it to admin panel routing.

---
+++ frontend/src/page/AdminPanelPage/StatisticsSection/StatisticsSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../AdminPanelStyles.css';
+
+
+function StatisticsSection() {
+    return (
+        <div className="adminPanelSection">
+        </div>
+    );
+}
+
+export default StatisticsSection;
