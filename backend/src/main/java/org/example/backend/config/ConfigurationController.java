27816b5adb6b6405abec09b51d83012f20fe66ad
gszp1 <grzybekmateusz@poczta.fm>
2024-06-21 01:59:10 +0200

    Adds endpoint for creating database.

---
+++ backend/src/main/java/org/example/backend/config/ConfigurationController.java
@@ -0,0 +1,35 @@
+package org.example.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+public class ConfigurationController {
+
+    private final ConfigurationService configurationService;
+
+    @Autowired
+    public ConfigurationController(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+
+    @PostMapping("/database/create")
+    public ResponseEntity<String> createDatabase(@RequestParam boolean withData) {
+        try {
+            if (withData) {
+                configurationService.createDatabaseWithExampleData();
+            } else {
+                configurationService.createEmptyDatabase();
+            }
+            return ResponseEntity.ok("Database created");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create database");
+        }
+    }
+}
--- backend/src/main/java/org/example/backend/config/ConfigurationService.java
+++ backend/src/main/java/org/example/backend/config/ConfigurationService.java
@@ -1,4 +1,37 @@
 package org.example.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Service
 public class ConfigurationService {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public ConfigurationService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    private static final String CREATE_STRING_NO_DATA = "sql/library_db.sql";
+
+    private static final String CREATE_STRING_WITH_DATA = "sql/library_db_example_data.sql";
+
+    public void createEmptyDatabase() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource(CREATE_STRING_NO_DATA));
+        }
+    }
+
+    public void createDatabaseWithExampleData() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource(CREATE_STRING_WITH_DATA));
+        }
+    }
 }
